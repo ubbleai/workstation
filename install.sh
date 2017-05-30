506850c8547c7f24b4c5673e0d371e6007e3b183
Michaël Journo <gobadiah@gmail.com>
2017-05-30 20:22:47 +0200

    Add install.sh script

---
+++ install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Password-less sudo
+
+echo "%staff ALL = (ALL) NOPASSWD: ALL" > /tmp/passwordless_sudo
+sudo chown 0 /tmp/passwordless_sudo
+sudo mv /tmp/passwordless_sudo /etc/sudoers.d/
+
+# Locate database
+
+sudo launchctl load -w /System/Library/LaunchDaemons/com.apple.locate.plist
+
+# Homebrew
+
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew tap caskroom/cask
+brew untap caskroom/cask
+
+# Chef
+
+brew cask install chefdk
+
+# Workstation
+
+cd /tmp/
+rm -rf workstation
+git clone https://github.com/gobadiah/workstation
+cd workstation
+berks install
+rm -rf ../cookbooks
+berks vendor ../cookbooks
+cd ..
+chef-client -z -l info -o 'recipe[workstation]'
