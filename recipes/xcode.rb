99bb4582bcc024c744e46c4e3d882775ed2e198e
Michaël Journo <gobadiah@gmail.com>
2017-06-13 12:33:31 +0200

    Comment out xcode recipe (install manually) + iterm2

--- recipes/xcode.rb
+++ recipes/xcode.rb
@@ -4,11 +4,12 @@
 #
 # Copyright (c) 2016 Michaël Journo, All Rights Reserved.
 
+# dmg_package 'Xcode_7' do
+#   app 'Xcode'
+#   volumes_dir 'Xcode'
+#   action :install
+# end
 
-dmg_package 'Xcode_7' do
-  app 'Xcode'
-  volumes_dir 'Xcode'
-  action :install
-end
+
 
 package 'xctool'
